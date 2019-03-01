d6f9b0b76a1ba4d99c153b012ee577932225ae00
iSnow <139699+iSnow@users.noreply.github.com>
2019-03-01 21:32:12 +0100

    Dynamic router config

--- src/main/java/de/isnow/sqlws/model/config/RouterConfig.java
+++ src/main/java/de/isnow/sqlws/model/config/RouterConfig.java
@@ -14,26 +14,16 @@ import java.util.TreeMap;
 
 @Data
 
-@JsonAutoDetect(
-        fieldVisibility = JsonAutoDetect.Visibility.ANY,
-        getterVisibility = JsonAutoDetect.Visibility.ANY,
-        setterVisibility = JsonAutoDetect.Visibility.NONE)
 @EqualsAndHashCode(of={"routes"})
 @ToString(of={"routes"})
-@JsonIgnoreProperties(ignoreUnknown = true)
-@JsonInclude
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public class RouterConfig {
 
     List<RouterConfigRecord> routes;
 
     @EqualsAndHashCode(of={"name", "path"})
     @ToString(of={"name", "path"})
-    @JsonIgnoreProperties(ignoreUnknown = true)
-    @JsonInclude
-    @JsonAutoDetect(
-            fieldVisibility = JsonAutoDetect.Visibility.ANY,
-            getterVisibility = JsonAutoDetect.Visibility.ANY,
-            setterVisibility = JsonAutoDetect.Visibility.NONE)
+    @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
     public static class RouterConfigRecord {
 
         String path;
@@ -42,6 +32,6 @@ public class RouterConfig {
 
         Map<String, String> components = new TreeMap<>();
 
-        Set<RouterConfig> children;
+        Set<RouterConfigRecord> children;
     }
 }
