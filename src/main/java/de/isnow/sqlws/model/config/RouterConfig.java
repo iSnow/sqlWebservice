4c2c002a42df06eb20ec0447342334f7bd21554e
iSnow <139699+iSnow@users.noreply.github.com>
2019-03-04 21:39:28 +0100

    working on dynamic components

--- src/main/java/de/isnow/sqlws/model/config/RouterConfig.java
+++ src/main/java/de/isnow/sqlws/model/config/RouterConfig.java
@@ -32,7 +32,7 @@ public class RouterConfig {
 
         Map<String, String> components = new TreeMap<>();
 
-        Map<String, Map> props = new TreeMap<>();
+        Map<String, List<Map>> props = new TreeMap<>();
 
         Set<RouterConfigRecord> children;
     }
